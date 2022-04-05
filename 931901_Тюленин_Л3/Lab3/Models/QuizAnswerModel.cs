690f44388da7807f7a8b5866f98ba6c61488ca44
TimofeyTyulenin <90829474+TimofeyTyulenin@users.noreply.github.com>
2022-04-05 22:16:32 +0700

    Delete 931901_Тюленин_Л3/Lab3 directory

--- 931901_Тюленин_Л3/Lab3/Models/QuizAnswerModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Lab3.Utils;
-
-namespace Lab3.Models {
-    public class QuizAnswerModel {
-        [LessThanOrNull(101, ErrorMessage = "слишком большое число!")]
-        public int? Answer { get; set; }
-    }
-}
