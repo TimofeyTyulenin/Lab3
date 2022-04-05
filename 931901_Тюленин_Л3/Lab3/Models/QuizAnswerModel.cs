13376b0aa70e0164de5a316196641f03814c6e89
TimofeyTyulenin <90829474+TimofeyTyulenin@users.noreply.github.com>
2022-04-05 22:15:22 +0700

    Add files via upload

---
+++ 931901_Тюленин_Л3/Lab3/Models/QuizAnswerModel.cs
@@ -0,0 +1,8 @@
+﻿using Lab3.Utils;
+
+namespace Lab3.Models {
+    public class QuizAnswerModel {
+        [LessThanOrNull(101, ErrorMessage = "слишком большое число!")]
+        public int? Answer { get; set; }
+    }
+}
